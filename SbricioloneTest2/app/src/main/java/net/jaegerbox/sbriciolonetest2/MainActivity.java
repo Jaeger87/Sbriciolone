35431076085f0c8561ba16d80bdd33e61bd17549
Unknown <bad_hunter_666@yahoo.it>
2018-09-10 00:48:51 +0200

    little improvements

--- SbricioloneTest2/app/src/main/java/net/jaegerbox/sbriciolonetest2/MainActivity.java
+++ SbricioloneTest2/app/src/main/java/net/jaegerbox/sbriciolonetest2/MainActivity.java
@@ -1,13 +1,52 @@
 package net.jaegerbox.sbriciolonetest2;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView tv1;
+    TextView tv2;
+    Switch eyeSwitch;
+    Switch mouthSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.textView);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        eyeSwitch = (Switch) findViewById(R.id.switch1);
+        eyeSwitch = (Switch) findViewById(R.id.switch2);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // COMPLETED (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        // COMPLETED (10) Return true to display your menu
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_backup) {
+            Context context = MainActivity.this;
+            String textToShow = "backup clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
